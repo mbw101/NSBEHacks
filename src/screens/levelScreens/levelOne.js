f7a6403206a51c5e63e1c3465ac1e5f47448d897
Malcolm Wright <wrightmbw01@gmail.com>
2024-02-25 07:39:07 -0500

    Got a basic quiz going!

--- src/screens/levelScreens/levelOne.js
+++ src/screens/levelScreens/levelOne.js
@@ -1,19 +1,120 @@
-import React from 'react';
-import logo from '../../fullCoinQuestLogo.png'
+import { React, useState } from 'react';
+import logo from '../../fullCoinQuestLogo.png';
+// import { QuestionGroup, Option, Question } from 'react-multiple-choice';
+
+const questions = [
+    "What type of card takes money directly from your account when making a purchase, debit or credit?",
+    "True or False: Credit cards allow you to borrow money up to a certain limit.",
+    "A _____________ is a safe place to store your spare change",
+    "What is income?",
+    "True or False: Bond accounts are a type of investment where you lend money to a company or government.",
+    "How should you formulate financial goals?",
+    "What is wealth?",
+    "A _____________ is an exchange of money or goods.",
+    "_____ goals are financial goals that are between 2-5 years in length"
+];
+
+const answers = [
+    ["Debit", "Credit"],
+    ["True", "False"],
+    ["Piggy Bank", "Mattress", "Chimney", "Back pocket"],
+    ["Money you spend", "Money you earn", "Money you save", "Money you invest"],
+    ["True", "False"],
+    ["SMART Method", "STAR Method", "SHORT Method", "All of these work"],
+    ["The amount of money you spend", "The total value of your assets minus your debts", "The number of transactions you make", "Your parents' garage"],
+    ["debit", "piggy bank", "goal", "transactions"],
+    ["short", "intermediate", "long term", "extended term"]
+];
+
+const correctAnswers = [
+    "Debit",
+    "True",
+    "Piggy bank",
+    "Money you earn",
+    "True",
+    "SMART Method",
+    "The total value of your assets minus your debts",
+    "transactions",
+    "Intermediate"
+];
 
 function changeBackground(color) {
     document.body.style.background = color;
-  }
-  
-  window.addEventListener("load", function() {changeBackground('#111e52')});
+}
 
-  export default function LevelOne(props) {
+window.addEventListener("load", function () { changeBackground('#111e52') });
+
+export default function LevelOne(props) {
+    const [questionIndex, setQuestionIndex] = useState(0);
+    const [question, setQuestion] = useState(questions[0]);
+    const [showFinish, setShowFinish] = useState(false);
+
+    const advanceQuestion = () => {
+        if (questionIndex >= questions.length - 1) {
+            // hide buttons and options
+            // show congratulations
+
+            setShowFinish(true);
+
+
+        }
+        setQuestionIndex(questionIndex + 1);
+        setQuestion(questions[questionIndex]);
+        // update answers
+
+    }
+
     return (
         <div className="levelOne">
-        <img
-            src={logo}
-            style={{ width: 527, height: 136, margin: "35px", marginTop: "400px" }}
+            <img
+                src={logo}
+                style={{ width: 527, height: 136, margin: "35px", marginTop: "400px" }}
             ></img>
+
+            <h1>Money Matters 101: Building a Strong Financial Foundation</h1>
+
+            <h3>Object:</h3>
+            <p>By the end of this lesson, students will be able to understand the basics of personal finance, including the concepts of saving, budgeting, and key financial goals.</p>
+
+
+            {
+                showFinish ?
+                    <p>Congratulations!!!</p>
+                    :
+                    <div id="">
+                        <input type="radio" name="question" value={answers[questionIndex][0]} /> <p>{answers[questionIndex][0]}</p>
+                        <input type="radio" name="question" value={answers[questionIndex][1]} /> <p>{answers[questionIndex][1]}</p>
+                        {
+                            answers[questionIndex].length > 2
+                                ?
+                                <div id="options">
+                                    <input type="radio" name="question" value={answers[questionIndex][2]} /> <p>{answers[questionIndex][2]}</p>
+                                    <input type="radio" name="question" value={answers[questionIndex][3]} /> <p>{answers[questionIndex][3]}</p>
+                                </div>
+                                :
+                                null
+                        }
+
+                        <button
+                            title="Next"
+                            onClick={() => advanceQuestion()}
+                            style={{
+                                width: 150,
+                                height: 90,
+                                backgroundColor: "#16f385",
+                                borderRadius: 30,
+                                border: "15px solid #13AE5E",
+                                //margin: "35px",
+                                color: "white",
+                                fontSize: "25px",
+                                fontWeight: "bold",
+                                fontFamily: "Monaco, monospace"
+                            }}>
+                            Next Question
+                        </button>
+                    </div>
+            }
+
         </div>
     );
 }
